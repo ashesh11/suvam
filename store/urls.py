4260444e9c74cf68e15fd91e25dbf442068d4d9e
Ashesh <ashesh.bh@gmail.com>
2022-04-12 22:25:09 +0545

    Contact Us page prepared

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,11 @@
 from django.urls import URLPattern, path
 from . import views
 
-urlpatterns = [
+urlpatterns =[
     path('', views.home, name="home"),
-    path('store', views.store, name="store" )
-    ]
+    path('store', views.store, name="store"),
+    path('aboutus', views.aboutus, name="aboutus"),
+    path('contact', views.contact, name="contact"),
+    path('promotions', views.promotions, name="promotions")
+]
+        
--- store/views.py
+++ store/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 from store.models import Product
 
 def home(request):
-    return render(request, 'home.html', )
+    return render(request, 'home.html')
+
+def aboutus(request):
+    return render(request, 'aboutus.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def promotions(request):
+    return render(request, 'promotions.html')
 
 def store(request):
     products= Product.objects.all()
